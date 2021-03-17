66434ec7e1ed64e694d6d5f7f1aa48a3b9868896
dorkele <dora_tominic@hotmail.com>
2021-03-17 16:55:17 +0100

    a few fixups

--- server/src/controllers/LoginController.ts
+++ server/src/controllers/LoginController.ts
@@ -1,1 +1,22 @@
-class LoginController {}
+import { Request, Response } from "express";
+import { get, controller } from "./decorators";
+
+@controller("/auth")
+class LoginController {
+    @get("/login")
+    getlogin(req: Request, res: Response): void {
+        res.send(`
+        <form method="POST">
+            <div>
+                <label>Email</label>
+                <input name="email" />
+            </div>
+            <div>
+                <label>Password</label>
+                <input name="password" type="password" />
+            </div>
+            <button>Submit</button>
+        </form>
+    `);
+    }
+}
